b8a59ab8b75f4867150eb79d3fd1e543942541b7
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-01-30 19:22:48 -0500

    small changes

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf("Here is y: %d\n", y);
     return 0;
 }
+
+/* I was not able to compile with optimization. 
+I went to ninja hours and they were not able to help me.
+
+It compiles when I run: gcc hello.c -o -v hello && ./hello
+But it has a linker error when I run: gcc hello.c -O2 -v hello && ./hello
+*/
