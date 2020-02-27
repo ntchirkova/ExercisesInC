f97df2013fe9f13eea40ca35e904767e9e9b882d
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-02-27 10:40:52 -0500

    fixed trout

---
+++ exercises/ex04/trout/trout.h
@@ -0,0 +1,49 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/un.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#define MAXLINE 4096
+#define BUFSIZE 1500
+
+typedef struct rec {                /* outgoing UDP data */
+  u_short seq;          /* sequence number */
+} Rec;
+
+typedef struct timeval Timeval;
+typedef struct sockaddr Sockaddr;
+
+extern Sockaddr *sasend;
+extern socklen_t salen;
+extern Sockaddr *sarecv;
+extern Sockaddr *salast;
+extern Sockaddr *sabind;
+extern int datalen;
+
+/* the following are a few definitions from Stevens' unp.h */
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+/* the following are prototypes for the Stevens utilities in util.c */
+
+void Sendto(int fd, const void *ptr, size_t nbytes, int flags,
+	    const struct sockaddr *sa, socklen_t salen);
+void loop_ttl ();
+
+
