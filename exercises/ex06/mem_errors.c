ff308964f070675d830ccaa16c32392612f76875
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-03-25 17:56:06 -0400

    no definite loss

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -15,13 +15,14 @@ void free_anything(int *p) {
 }
 
 int read_element(int *array, int index) {
-    int x = array[index];
-    return x;
+    int *x = malloc(sizeof (int));
+    *x = array[index];
+    return *x;
 }
 
 int main()
 {
-    int never_allocated;
+    int *never_allocated = malloc(sizeof (int));
     int *free_twice = malloc(sizeof (int));
     int *use_after_free = malloc(sizeof (int));
     int *never_free = malloc(sizeof (int));
@@ -29,28 +30,26 @@ int main()
     int *array2 = malloc(100 * sizeof (int));
 
     // valgrind does not bounds-check static arrays
-    read_element(array1, -1);
-    read_element(array1, 100);
+    array2[1] = 3;
+    printf("%i", read_element(array1, 1));
 
     // but it does bounds-check dynamic arrays
-    read_element(array2, -1);
-    read_element(array2, 100);
+    // read_element(array2, -1);
+    // read_element(array2, 100);
 
     // and it catches use after free
-    free(use_after_free);
     *use_after_free = 17;
 
     // never_free is definitely lost
     *never_free = 17;
 
     // the following line would generate a warning
-    // free(&never_allocated);
+    *never_allocated = 6;
 
-    // but this one doesn't
-    free_anything(&never_allocated);
-
-    free(free_twice);
-    free(free_twice);
+    free(array2);
+    free_anything(use_after_free);
+    free_anything(never_free);
+    free_anything(never_allocated);
 
     return 0;
 }
